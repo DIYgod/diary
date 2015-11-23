b461b24e71f94020328fa43ed5b12ec23330ba27
DIYgod <diy.d.god@gmail.com>
2015-11-23 22:59:49 +0800

    init DIYgod

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   author: {
-    name: "Joyee Cheung",
-    url: "https://github.com/joyeecheung"
+    name: "DIYgod",
+    url: "https://github.com/DIYgod"
   },
   repo: {  // for the markdown source
-    content: "https://github.com/joyeecheung/my-tech-diary",
+    content: "https://github.com/DIYgod/Demeter",
     as_submodule: 'diary'
   },
   base_url: '/diary',  // remote directory for the website
-  ga_id: 'UA-63818301-1',  // Optional: Universal Google Analytics ID
-  title: 'My Tech Diary'
+  ga_id: 'UA-48084758-2',  // Optional: Universal Google Analytics ID
+  title: 'Demeter'
 }
